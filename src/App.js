bdd220b8973555ae0e2971496fcbc1077633cf8e
Nguyễn Văn Quyết <chiengaphuly299@gmail.com>
2024-04-04 20:55:17 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Fragment, useState } from "react";
+import Header from "./component/Header";
+import TableUser from "./component/TableUser";
+import Container from "react-bootstrap/Container";
+import ModalAddNew from "./component/ModalAddNew";
 
 function App() {
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="app-container">
+        <Header />
+        <Container>
+          <div className="my-3 d-flex justify-content-between">
+            <h4>
+              <b>List User</b>
+            </h4>
+            <button
+              className="btn btn-primary"
+              onClick={() => setIsShowModalAddNew(true)}
+            >
+              Add new user
+            </button>
+          </div>
+          <TableUser />
+        </Container>
+        <ModalAddNew show={isShowModalAddNew} handleClose={handleClose} />
+      </div>
+    </Fragment>
   );
 }
 
